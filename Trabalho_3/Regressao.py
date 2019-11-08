2f6b474a413c20a4cb292fb6dd4ddf2f764c21f5
Leonardo Rovigo <lrovigo@furb.br>
2019-11-08 20:09:42 -0300

    começo da implementação da correlação

---
+++ Trabalho_3/Regressao.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x1 = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
+y1 =  [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
+
+x2 = [10,8,13,9,11,14,6,4,12,7,5]
+y2 = [9.14, 8.14, 8.47, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
+
+x3 = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 19]
+y3 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 5.56, 7.91, 6.89, 12.50]
+
+
+#
+
+
+def correlacao(Nx,Ny):
+
+    media_x = (avg(Nx))
+    media_y = (avg(Ny))
+    soma = 0
+    for(i in range(len(Nx))):
+        soma_media_x_menos_media_x = retornar_menos_media(Nx[i],media_x)  
+        soma_media_y_menos_media_y = retornar_menos_media(Ny[i], media_y)
+        soma += soma_media_x_menos_media_x * soma_media_y_menos_media_y
+
+    #for(i in range(len(Nx))):
+    #    soma =  
+    r = soma_media_x_menos_media_x * soma_media_y_menos_media_y /    
+
+
+def regressao:
+    
+
+
+
+def retornar_menos_media(n,media):
+    soma_media_menos_media = n - media
+    return soma_media_menos_media;
