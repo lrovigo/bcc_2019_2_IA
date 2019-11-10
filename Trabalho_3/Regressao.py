0529881054370e9c40a223150e40e604d9a3c26d
Leonardo Rovigo <leonardorovigo@gmail.com>
2019-11-10 20:50:00 -0300

    implmentado a correlãção

--- Trabalho_3/Regressao.py
+++ Trabalho_3/Regressao.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from statistics import mean 
+import math  
 
 x1 = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
 y1 =  [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
@@ -11,30 +12,32 @@ y2 = [9.14, 8.14, 8.47, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
 x3 = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 19]
 y3 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 5.56, 7.91, 6.89, 12.50]
 
-
-#
-
+def main():
+    print(correlacao(x1,y1))    
 
 def correlacao(Nx,Ny):
 
-    media_x = (avg(Nx))
-    media_y = (avg(Ny))
+    media_x = (mean(Nx))
+    media_y = (mean(Ny))
     soma = 0
-    for(i in range(len(Nx))):
-        soma_media_x_menos_media_x = retornar_menos_media(Nx[i],media_x)  
-        soma_media_y_menos_media_y = retornar_menos_media(Ny[i], media_y)
-        soma += soma_media_x_menos_media_x * soma_media_y_menos_media_y
+    soma_x = 0
+    soma_y = 0
+    for i in range(len(Nx)):
+        x = (Nx[i] - media_x)
+        y = (Ny[i] - media_y)
+        soma += x * y
+        
+    for i in range(len(Nx)):
+        soma_x += (Nx[i] - media_x)**2
+        soma_y += (Ny[i] - media_y)**2
+    soma_2 = math.sqrt(soma_x * soma_y)    
 
-    #for(i in range(len(Nx))):
-    #    soma =  
-    r = soma_media_x_menos_media_x * soma_media_y_menos_media_y /    
+    r = soma / soma_2
+    return r
 
-
-def regressao:
+def regressao():
+    print('regrediu')
     
-
-
-
-def retornar_menos_media(n,media):
-    soma_media_menos_media = n - media
-    return soma_media_menos_media;
+    
+if __name__ == '__main__':
+    main()
