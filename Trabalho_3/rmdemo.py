39201c7168c3ebd3298a44807fba5fa25b7ee53b
Gustavo Henrique Spiess <gustavo.spiess@philips.com>
2019-11-22 20:18:34 -0300

    parte 2 trabalho 3 quase lá

--- Trabalho_3/rmdemo.py
+++ Trabalho_3/rmdemo.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
 from scipy.io import loadmat
 import numpy as np
 
@@ -12,6 +16,7 @@ def get_data(file_path):
 
     return data
 
+
 def main():
     data = get_data('Dados/data.mat')
 
@@ -19,8 +24,48 @@ def main():
     y = np.array(tuple((house[2],) for house in data))
 
     beta = calculate_betas(x, y)
+    # plot_points(data)
+    plot_line(beta, data)
 
-    print(beta)
+    plt.show()
+
+
+def plot_points(data):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    scatter_data = zip(*tuple(map(tuple, data)))
+    ax.scatter(*scatter_data, c='r', marker='^')
+
+
+def plot_line(beta, data):
+    
+    size_list = tuple(house[0] for house in data)
+    qt_room_list = tuple(house[1] for house in data)
+
+    max_size = max(size_list)
+    min_size = min(size_list)
+    max_qt_room = max(qt_room_list)
+    min_qt_room = min(qt_room_list)
+
+    x = np.arange(min_size, max_size, 100)
+    y = np.arange(min_qt_room, max_qt_room+1, 1)
+    x, y = np.meshgrid(x, y)
+    z = beta[0] + x * beta[1] + y * beta[2]
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    scatter_data = zip(*tuple(map(tuple, data)))
+    ax.scatter(*scatter_data, c='r', marker='^', label='treinamento')
+    ax.legend()
+
+    ax.plot_surface(x, y, z, cmap='viridis', label='expectativa de preço')
+
+    ax.set_xlabel('tamanho')
+    ax.set_ylabel('quantidade de quartos')
+    ax.set_zlabel('preço')
+
+
 
 def calculate_betas(x, y):
     tra_x = np.transpose(x)
@@ -30,5 +75,10 @@ def calculate_betas(x, y):
 
     return tuple(float(x) for x in betas)
 
+def plot():
+    data = get_data('Dados/data.mat')
+
+
 if __name__ == '__main__':
     main()
+
